9f94d7fd73dba2c2ffe30be860b34dc0164f8972
Gilad Segal <gilads@wix.com>
2018-10-25 16:23:12 +0300

    typings

--- src/dataService.ts
+++ src/dataService.ts
@@ -1,67 +1,49 @@
 import * as Abstract from 'abstract-sdk';
 
-
-
-
-// async function foo() {
-//   const dataService = new DataService(abstract);
-
-//   const projects = await dataService.getAllProjects();
-//   //   projects.map(project => {
-//   //     console.log(`${project.name}: ${project.id}`);
-//   //   });
-
-//   const project = await dataService.getProject(projects, {
-//     name: 'Subscriptions'
-//   });
-
-//   const branch = await abstract.branches.info({
-//     projectId: project.id,
-//     branchId: 'master'
-//   });
-
-//   const commits = await dataService.getBranch({
-//     projectId: project.id,
-//     branchId: 'master'
-//   });
-
-//   console.log(commits);
-// }
-
 export class DataService {
   constructor(private readonly abstract: any) {
     const accessToken =
       '5c444fc40c2a5124a7aab5fcba89dfd7536ce7072cc05e973d8eeedbbc08620b';
 
-    this.abstract = abstract || Abstract.Client({
-      accessToken,
-      transport: Abstract.TRANSPORTS.API
-    });
+    this.abstract =
+      abstract ||
+      Abstract.Client({
+        accessToken,
+        transport: Abstract.TRANSPORTS.API
+      });
   }
 
-  getAllProjects = () => {
+  getAllProjects = (): Abstract.Project[] => {
     return this.abstract.projects.list();
-  }
+  };
 
-  getProject = (projects: any[], {name}: {name: string}) => {
+  getProject = (
+    projects: Abstract.Project[],
+    {name}: {name: string}
+  ): Abstract.Project | undefined => {
     return projects.find(project => project.name === name);
-  }
+  };
 
-  getBranch = ({projectId, branchId}: {projectId: string, branchId: string}) => {
+  getBranch = ({
+    projectId,
+    branchId
+  }: Abstract.BranchDescriptor): Abstract.Branch => {
     return this.abstract.branches.info({
       projectId,
       branchId
     });
-  }
+  };
 
-  getCommits = ({projectId, branchId}: {projectId: string, branchId: string}) => {
+  getCommits = ({
+    projectId,
+    branchId
+  }: Abstract.CommitDescriptor): Abstract.Commit[] => {
     return this.abstract.commits.list({
       projectId,
       branchId
     });
-  }
+  };
 }
-
 
 //   const commits = await dataService.getBranch({
 //     projectId: project.id,
--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,121 @@
-declare module 'abstract-sdk';
+declare module 'abstract-sdk' {
+  namespace AbstractSdk {
+    export enum TRANSPORTS {
+      API,
+      CLI
+    }
+    export function Client(params: {
+      accessToken: string;
+      transport: TRANSPORTS;
+    }): any;
+
+    export type Branch = {
+      createdAt: string;
+      description: string;
+      divergedFromBranchId: string;
+      head: string;
+      id: string;
+      mergeSha: string;
+      mergedIntoBranchId: string;
+      name: string;
+      parent: string;
+      projectId: string;
+      startedAtSha: string;
+      status: BranchStatus;
+      updatedAt: string;
+      userId: string;
+      userName: string;
+    };
+
+    export type BranchStatus =
+      | 'active'
+      | 'wip'
+      | 'feedback'
+      | 'review'
+      | 'merged'
+      | 'archived'
+      | 'deleted'
+      | 'diverged';
+
+    export type Commit = {
+      description: string;
+      destinationBranchId: string;
+      destinationBranchName: string;
+      fileIds: string[];
+      parents: string[];
+      projectId: string;
+      sha: string;
+      sourceBranchId: string;
+      sourceBranchName: string;
+      time: string;
+      title: string;
+      type: CommitType;
+      userId: string;
+      userName: string;
+    };
+
+    export type CommitType =
+      | 'NORMAL'
+      | 'PROJECT_CREATED'
+      | 'FILE_ADDED'
+      | 'FILE_RENAMED'
+      | 'FILE_DELETED'
+      | 'FILE_REPLACED'
+      | 'LIBRARY_ADDED'
+      | 'LIBRARY_REMOVED'
+      | 'RESTORE'
+      | 'UPDATE'
+      | 'MERGE';
+
+    export type Project = {
+      about: string;
+      archivedAt: string;
+      color: string;
+      createdAt: string;
+      createdByUser: any;
+      description: string;
+      firstPushedAt: string;
+      id: string;
+      name: string;
+      organizationId: string;
+      pushedAt: string;
+      repoCreatedAt: string;
+      sizeInBytes: number;
+      updatedAt: string;
+      visibility: 'organization' | 'specific';
+    };
+
+    export type ProjectDescriptor = {
+      projectId: string;
+    };
+
+    export type BranchDescriptor = {
+      projectId: string;
+      branchId: string;
+    };
+
+    export type CommitDescriptor = {
+      projectId: string;
+      branchId: string | 'master';
+      sha?: string;
+    };
+  }
+
+  export = AbstractSdk;
+}
+
+/*
+export = WixEventually;
+
+declare function WixEventually(fn: Function, opts?: WixEventually.Opts): Promise<void>;
+
+declare namespace WixEventually {
+  export interface Opts {
+    timeout?: number;
+    interval?: number;
+  }
+
+  function _with(overrides: Opts): (fn: Function, opts?: WixEventually.Opts) => Promise<void>;
+  export { _with as with }
+}
+*/
